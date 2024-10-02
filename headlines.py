cf3f8d7f6a70d50d611940656c5e2fc187cc92e1
rosefinchh <nelsonritz55@gmail.com>
2024-10-02 22:03:28 +0530

    added routes

---
+++ headlines.py
@@ -0,0 +1,42 @@
+import feedparser
+from flask import Flask
+
+app = Flask(__name__)
+
+RSS_FEEDS = {
+    "bbc": "https://feeds.bbci.co.uk/news/technology/rss.xml",
+    "fox": "https://moxie.foxnews.com/google-publisher/sports.xml",
+    "iol": "https://rss.iol.io/iol/sunday-tribune"
+    }
+
+
+@app.route("/")
+@app.route("/bbc")
+def bbc():
+    return get_news("bbc")
+    
+@app.route("/fox")
+def cnn():
+    return get_news("fox")
+
+@app.route("/iol")
+def iol():
+    return get_news("iol")
+
+
+def get_news(publication):
+    feed = feedparser.parse(RSS_FEEDS[publication])
+    first_article=feed["entries"][0]
+    return """
+<html>
+	<body>
+		<h1> HEADLINES </h1>
+		<b>{0}</b> <br/>
+		<i>{1}</i> <br/>
+		<p>{2}</p> <br/>
+	</body>
+</html>""".format(first_article.get("title"), first_article.get("published"),
+                  first_article.get("summary"))
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
